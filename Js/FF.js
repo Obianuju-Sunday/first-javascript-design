acbd151db6ae9d5c79cd96225cf1009e436a6cd8
Simon Ugorji <ugorji757@gmail.com>
2022-12-16 12:37:35 +0100

    Fixed the function

--- Js/FF.js
+++ Js/FF.js
@@ -21,15 +21,15 @@ navBar.forEach(function(a){
 // counter design
 document.addEventListener("DOMContentLoaded", () => {
     function counter(id, start, end, duration){
-        let obj = document.getElementById(id),
-        current = start,
-        range = end - start,
-        increment = end > start ? 1 : -1,
-        step = Math.abs(Math.floor(duration / range));
-        timer = setInterval(() => {
+        let obj = document.getElementById(id);
+        let current = start; //0
+        let range = end - start; //750
+        let increment = end > start ? 1 : -1; //1
+        let step = Math.abs(Math.floor(duration / range)); //1
+        let timer = setInterval(() => {
             current += increment;
             obj.textContent = current;
-            if(current == end){
+            if(current >= end){
                 clearInterval(timer);
             }
         }, step);
